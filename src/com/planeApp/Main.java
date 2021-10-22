3a571724f6482560eb9c88d9d2114fcc1f49e66d
zahir679 <zahirahmed0512@gmail.com>
2021-10-22 15:44:48 +0100

    Created user interface

--- src/com/planeApp/Main.java
+++ src/com/planeApp/Main.java
@@ -2,6 +2,7 @@ package com.planeApp;
 
 import java.util.Arrays;
 import java.util.Random;
+import java.util.Scanner;
 import java.util.UUID;
 
 public class Main {
@@ -12,9 +13,10 @@ public class Main {
         Passport passport2 = new Passport(35652567, Nationality.ITALIAN );
         Passport passport3 = new Passport(35652567, Nationality.SPANISH);
 
-        User user1 = new User("Jason", "jasonsum321@gmail.com", 07481555888F, UUID.randomUUID().toString(), passport1);
-        User user2 = new User("Zahir", "zahir123@gmail.com", 07451255888F, UUID.randomUUID().toString(), passport2);
-        User user3 = new User("Laiba", "laiba@gmail.com", 07381955682F, UUID.randomUUID().toString(), passport3);
+//        User user1 = new User("Jason", "jasonsum321@gmail.com", 07481555888F, UUID.randomUUID().toString(), passport1);
+//        User user2 = new User("Zahir", "zahir123@gmail.com", 07451255888F, UUID.randomUUID().toString(), passport2);
+//        User user3 = new User("Laiba", "laiba@gmail.com", 07381955682F, UUID.randomUUID().toString(), passport3);
+
 
         Flights flight1 = new Flights("London, UK to Tenerife, Spain");
         Flights flight2 = new Flights("London, UK to Venice, Italy");
@@ -25,7 +27,8 @@ public class Main {
         Flights[] flights = {flight1, flight2, flight3, flight4, flight5}; // should be initially an empty array where userinput adds it into array.
         FlightsService flightsService = new FlightsService();
 
-        flightsService.addFlight(flight1);
-        System.out.println(flightsService.bookedFlights);
+//        flightsService.addFlight(flight1);
+//        System.out.println(flightsService.bookedFlights);
+        userService.makeUser();
     }
 }
---
+++ src/com/planeApp/PassportService.java
@@ -0,0 +1,4 @@
+package com.planeApp;
+
+public class PassportService {
+}
